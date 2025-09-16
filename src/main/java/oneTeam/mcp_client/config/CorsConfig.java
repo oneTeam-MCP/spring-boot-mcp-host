4725c197ac6032123bdc58327b6ce7a1b8d8aef8
shash0423 <shash042319@naver.com>
2025-09-16 17:11:52 +0900

    chore: CORS 포트 변경

--- src/main/java/oneTeam/mcp_client/config/CorsConfig.java
+++ src/main/java/oneTeam/mcp_client/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns(
-                        "http://localhost:3000",
+                        "http://localhost:5173",
                         "https://oneteam-mcp.site",
                         "https://*.oneteam-mcp.site"
                 )
